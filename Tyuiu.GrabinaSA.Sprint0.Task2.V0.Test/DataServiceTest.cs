3133dbd96e2dcbd30f4e6270ef86f1200c69ca07
GrabinaSergey <dieumortx@gmail.com>
2024-09-19 21:01:53 +0500

    Task2

---
+++ Tyuiu.GrabinaSA.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+using Tyuiu.GrabinaSA.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.GrabinaSA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Сергей";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет Сергей", res);
+        }
+    }
+}
---
+++ Tyuiu.GrabinaSA.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.GrabinaSA.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.GrabinaSA.Sprint0.Task2.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Сергей"));
+            Console.ReadKey();
+            Console.ReadKey();
+
+        }
+    }
+}
